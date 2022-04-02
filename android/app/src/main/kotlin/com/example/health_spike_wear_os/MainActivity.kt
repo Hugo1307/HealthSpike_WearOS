60403f72a2d5dc428e2a2ed6468a6ef764ad1a45
Hugo1307 <hugomiguel_2001_@sapo.pt>
2022-04-03 00:04:45 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/health_spike_wear_os/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.health_spike_wear_os
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:wear/wear.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Wear App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: WatchScreen(),
+        debugShowCheckedModeBanner: false,
+      );
+}
+
+class WatchScreen extends StatelessWidget {
+  const WatchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: WatchShape(
+            builder: (BuildContext context, WearShape shape, Widget? child) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Shape: ${shape == WearShape.round ? 'round' : 'square'}',
+                  ),
+                  child!,
+                ],
+              );
+            },
+            child: AmbientMode(
+              builder: (BuildContext context, WearMode mode, Widget? child) {
+                return Text(
+                  'Mode: ${mode == WearMode.active ? 'Active' : 'Ambient'}',
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
